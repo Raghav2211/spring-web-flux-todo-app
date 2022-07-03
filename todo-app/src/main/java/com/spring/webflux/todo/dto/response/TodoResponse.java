5a085056ae96b14a43874fc026ecad5252e3a192
Raghav2211 <rghvj.22@gmail.com>
2022-07-03 21:05:07 +0530

    add isActive in todoResponse

--- todo-app/src/main/java/com/spring/webflux/todo/dto/response/TodoResponse.java
+++ todo-app/src/main/java/com/spring/webflux/todo/dto/response/TodoResponse.java
@@ -7,11 +7,14 @@ import lombok.Getter;
 @Getter
 public class TodoResponse extends TodoRequest {
   private final String id;
+  private final Boolean isActive;
   private final String sectionId;
 
-  public TodoResponse(String id, String sectionId, String task, Schedule schedule) {
+  public TodoResponse(
+      String id, String sectionId, String task, Schedule schedule, Boolean isActive) {
     super(task, schedule);
     this.id = id;
     this.sectionId = sectionId;
+    this.isActive = isActive;
   }
 }
