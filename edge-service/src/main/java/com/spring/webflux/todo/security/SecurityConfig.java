8560754907fa56bb94c8f91d5725674808f84bc1
Raghav2211 <rghvj.22@gmail.com>
2022-07-04 23:37:16 +0530

    fix security loop

--- edge-service/src/main/java/com/spring/webflux/todo/security/SecurityConfig.java
+++ edge-service/src/main/java/com/spring/webflux/todo/security/SecurityConfig.java
@@ -1,34 +1,43 @@
 package com.spring.webflux.todo.security;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.security.web.server.WebFilterChainProxy;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 
-@EnableWebFluxSecurity
 @Configuration
+@ConditionalOnClass({EnableWebFluxSecurity.class, WebFilterChainProxy.class})
+@ConditionalOnMissingBean({SecurityWebFilterChain.class, WebFilterChainProxy.class})
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@AutoConfigureBefore(ReactiveSecurityAutoConfiguration.class)
 public class SecurityConfig {
 
   @Bean
-  SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-    return http.csrf()
-        .disable()
-        .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
-        .authorizeExchange()
-        .pathMatchers(
-            // spotless:off
-
-            // "/actuator/info", <-- https://github.com/spring-projects/spring-boot/issues/24205
-
-            // spotless:on
-            "/actuator/health", "/favicon.ico")
+  SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    return http.authorizeExchange()
+        .pathMatchers("/actuator/health", "/favicon.ico")
         .permitAll()
+        .anyExchange()
+        .authenticated()
         .and()
-        .authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
-        .oauth2Login(Customizer.withDefaults())
+        .httpBasic()
+        .and()
+        .formLogin()
+        .and()
+        .oauth2Login()
+        .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(
+            new RedirectServerAuthenticationEntryPoint("/oauth2/authorization/gateway"))
+        .and()
         .build();
   }
 }
