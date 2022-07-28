b117185fce62e29ba6f6cdef095b2441b6d05112
Raghav <7526431+Raghav2211@users.noreply.github.com>
2022-07-28 19:27:59 +0530

    Persistent session store (#8)
    * initial commit
    
    * add redis dependency
    
    * add redis implementation
    
    * fix ci
    
    * add client-name
    
    * separate maven/gradle pipeline
    
    * correct pipeline name
    
    * gradle pipeline
    
    * fix integration test
    
    * correct dependencies in maven build
    
    * correct pom structure
    
    * add docker-compose.yaml
    
    * correct working_dir
    
    * add timeout in build & separate test build sh fro maven , gradle
    
    Co-authored-by: Raghav2211 <rghvj.22@gmail.com>

---
+++ edge-service/src/main/java/com/spring/webflux/todo/security/RedisReactiveOAuth2AuthorizedClientService.java
@@ -0,0 +1,55 @@
+package com.spring.webflux.todo.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RMapReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Primary
+@RequiredArgsConstructor
+@Slf4j
+public class RedisReactiveOAuth2AuthorizedClientService
+    implements ReactiveOAuth2AuthorizedClientService {
+  private final RedissonReactiveClient redissonReactiveClient;
+
+  @Override
+  public Mono<OAuth2AuthorizedClient> loadAuthorizedClient(
+      String clientRegistrationId, String principalName) {
+    log.info(
+        "loadAuthorizedClient with clientRegistrationId {} & principalName {}",
+        clientRegistrationId,
+        principalName);
+    RMapReactive<String, OAuth2AuthorizedClient> authorizedClient =
+        redissonReactiveClient.getMap(principalName);
+    return authorizedClient.get(clientRegistrationId);
+  }
+
+  @Override
+  public Mono<Void> saveAuthorizedClient(
+      OAuth2AuthorizedClient authorizedClient, Authentication principal) {
+    log.info(
+        "saveAuthorizedClient with clientRegistrationId {} & principalName {}",
+        authorizedClient.getClientRegistration().getRegistrationId(),
+        principal.getName());
+    return redissonReactiveClient
+        .getMap(principal.getName())
+        .put(authorizedClient.getClientRegistration().getRegistrationId(), authorizedClient)
+        .then();
+  }
+
+  @Override
+  public Mono<Void> removeAuthorizedClient(String clientRegistrationId, String principalName) {
+    log.info(
+        "removeAuthorizedClient with clientRegistrationId {} & principalName {}",
+        clientRegistrationId,
+        principalName);
+    return redissonReactiveClient.getMap(principalName).remove(clientRegistrationId).then();
+  }
+}
--- edge-service/src/test/java/com/spring/webflux/todo/EdgeApplicationTests.java
+++ edge-service/src/test/java/com/spring/webflux/todo/EdgeApplicationTests.java
@@ -1,13 +1,53 @@
 package com.spring.webflux.todo;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.config.Config;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest
+@Import({EdgeApplicationTests.TestConfig.class})
 @ActiveProfiles("test")
+@Testcontainers
 class EdgeApplicationTests {
+  public static final int REDIS_PORT = 6379;
+
+  @Container
+  public static final GenericContainer REDIS =
+      new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+          .waitingFor(Wait.forListeningPort())
+          .withExposedPorts(REDIS_PORT);
 
   @Test
   void contextLoads() {}
+
+  @TestConfiguration
+  static class TestConfig {
+
+    @Bean
+    @Primary
+    public RedissonReactiveClient redissonReactiveClient() {
+      var config = new Config();
+      config
+          .useSingleServer()
+          .setAddress(
+              String.format(
+                  "redis://%s:%s",
+                  REDIS.getContainerIpAddress(), REDIS.getMappedPort(REDIS_PORT).toString()));
+      return Redisson.create(config).reactive();
+    }
+  }
 }
